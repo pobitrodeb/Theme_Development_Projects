87958683f20756cfceced63d7e93c230c0769de8
Pobitro Deb <pobitrodebnath85@gmail.com>
2024-09-16 04:21:02 +0600

    CSS support added

--- Project_01/wp-content/themes/simplekeyapth/index.php
+++ Project_01/wp-content/themes/simplekeyapth/index.php
@@ -5,16 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home | Simple Project</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/responsive.css">
 </head>
 <body>
     <div class="area">
         <!-- Header Start Here -->
         <div class="header fix">
             <div class="logo">
-                <a href="index.html">
-                    <img src="img/logo.png" alt="Logo">
+                <a href="index.php">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="Logo">
                 </a>
             </div>
             <div class="menu">
@@ -31,7 +31,7 @@
 
         <!-- Banner Start Here -->
         <div class="banner">
-            <img src="img/bannar.jpg" alt="Banner">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/bannar.jpg" alt="Banner">
         </div>
         <!-- Banner End Here -->
         
@@ -43,19 +43,19 @@
             <div class="service fix">
                 <div class="single-service">
                     <h4>service title</h4>
-                    <img src="img/service1.jpg" alt="Service 1">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/service1.jpg" alt="Service 1">
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae harum quidem labore architecto optio doloremque similique ab, mollitia distinctio eum.</p>
                     <a href="" class="btn">read more</a>
                 </div>
                 <div class="single-service">
                     <h4>service title</h4>
-                    <img src="img/service2.jpg" alt="Service 2">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/service2.jpg" alt="Service 2">
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae harum quidem labore architecto optio doloremque similique ab, mollitia distinctio eum.</p>
                     <a href="" class="btn">read more</a>
                 </div>
                 <div class="single-service">
                     <h4>service title</h4>
-                    <img src="img/service3.jpg" alt="Service 3">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/service3.jpg" alt="Service 3">
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae harum quidem labore architecto optio doloremque similique ab, mollitia distinctio eum.</p>
                     <a href="" class="btn">read more</a>
                 </div>
