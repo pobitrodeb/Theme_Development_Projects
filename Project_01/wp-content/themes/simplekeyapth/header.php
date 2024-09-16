1f17c651f4cc0b263488088ecb4c0f8b878b4ffa
Pobitro Deb <pobitrodebnath85@gmail.com>
2024-09-16 07:46:24 +0600

    logo url dynamic added

--- Project_01/wp-content/themes/simplekeyapth/header.php
+++ Project_01/wp-content/themes/simplekeyapth/header.php
@@ -12,7 +12,7 @@
         <!-- Header Start Here -->
         <div class="header fix">
             <div class="logo">
-                <a href="index.php">
+                <a href="<?php echo site_url(); ?>">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="Logo">
                 </a>
             </div>
