0623f34904f8b370da6941b8febd726cb94adc5c
Pobitro Deb <pobitrodebnath85@gmail.com>
2024-09-16 06:54:25 +0600

    Sidebar font-end show added

--- Project_01/wp-content/themes/simplekeyapth/sidebar.php
+++ Project_01/wp-content/themes/simplekeyapth/sidebar.php
@@ -1,10 +1,8 @@
 <div class="single-sidebar">
-    <h4>sidebar menu</h4>
-    <ul>
-        <li><a href="index.html">Home</a></li>
-        <li><a href="about.html">About</a></li>
-        <li><a href="services.html">Services</a></li>
-        <li><a href="gallery.html">Gallery</a></li>
-        <li><a href="">Contact</a></li>
-    </ul>
+    <?php if(is_active_sidebar('sidebar-1')){
+        dynamic_sidebar('sidebar-1');
+    }
+
+    ?>
+    
 </div>
