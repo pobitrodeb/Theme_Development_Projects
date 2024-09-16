353071a0facc5367f0feacbccf319e2adcb71f78
Pobitro Deb <pobitrodebnath85@gmail.com>
2024-09-16 06:35:38 +0600

    Sidebar register added

--- Project_01/wp-content/themes/simplekeyapth/functions.php
+++ Project_01/wp-content/themes/simplekeyapth/functions.php
@@ -11,10 +11,29 @@ function simplekeyapth_setup(){
 }
 add_action('after_setup_theme', 'simplekeyapth_setup');
 
-function simplekeyapth(){
+function simplekeyapth_style(){
     wp_enqueue_style('responsive', get_template_directory_uri() . '/assets/css/responsive.css', array(), '10.0', 'all');
     wp_enqueue_style('main', get_template_directory_uri() . '/assets/css/style.css', array(), '10.0', 'all');
     // wp_enqueue_style('main', get_stylesheet_uri( )); **This is theme CSS 
    
 }
-add_action('wp_enqueue_scripts', 'simplekeyapth');
+add_action('wp_enqueue_scripts', 'simplekeyapth_style');
+
+
+
+// Register Sidebar 
+function simplekeyapth_widgets(){
+
+    register_sidebar(array(
+        'name'              => __('Main Sidebar', 'simplekeyapth'), 
+        'id'                => ('sidebar-1'), 
+        'description'       => __('Our Main Sidebar', 'simplekeyapth'), 
+        'before_widget'     => '<div class="single-sidebar">', 
+        'after_widget'      => '</div>', 
+        'before_widget'     => '<h4>', 
+        'after_widget'     => '</h4>', 
+    )); 
+
+}
+
+add_action('widgets_init', 'simplekeyapth_widgets');
--- Project_01/wp-content/themes/simplekeyapth/sidebar.php
+++ Project_01/wp-content/themes/simplekeyapth/sidebar.php
@@ -1,8 +1,10 @@
-<h4>sidebar menu</h4>
-                <ul>
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="about.html">About</a></li>
-                    <li><a href="services.html">Services</a></li>
-                    <li><a href="gallery.html">Gallery</a></li>
-                    <li><a href="">Contact</a></li>
-                </ul>
+<div class="single-sidebar">
+    <h4>sidebar menu</h4>
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="services.html">Services</a></li>
+        <li><a href="gallery.html">Gallery</a></li>
+        <li><a href="">Contact</a></li>
+    </ul>
+</div>
