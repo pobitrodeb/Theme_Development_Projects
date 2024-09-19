31b0fddd5fecd82739bd426c49e620c3358880fe
Pobitro Deb <pobitrodebnath85@gmail.com>
2024-09-19 06:21:42 +0600

    Register nav menus added

--- Project_02_Blog/wp-content/themes/blogkyeapth/functions.php
+++ Project_02_Blog/wp-content/themes/blogkyeapth/functions.php
@@ -1,5 +1,18 @@
 <?php 
 
+function blogkeyapth_setup(){
+
+   register_nav_menus(array(
+    'main-menu' => 'Main Menu', 
+    'footer-menu' => 'Footer Menu', 
+   ));
+
+}
+
+add_action('after_setup_theme', 'blogkeyapth_setup');
+
+
+
 function blogkeyapth_scripts(){
 
      wp_enqueue_style('font-awesome', get_template_directory_uri() . '/assets/css/font-awesome.min.css', array(), '1.0.0', 'all');
