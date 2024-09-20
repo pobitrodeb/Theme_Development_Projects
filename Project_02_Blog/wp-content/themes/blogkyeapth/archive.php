838a73a9205f0421fe89093896150fccaee5adda
Pobitro Deb <pobitrodebnath85@gmail.com>
2024-09-20 08:39:51 +0600

    archive/category page added

---
+++ Project_02_Blog/wp-content/themes/blogkyeapth/archive.php
@@ -0,0 +1,17 @@
+
+<?php get_header(); ?> 
+
+<?php get_template_part('template-parts/content', 'banner'); ?> 
+
+<?php get_template_part('template-parts/content', 'services'); ?> 
+
+ <!-- Blog Start Here -->
+ <div class="blogs">
+     <div class="blog-left">
+         <h4>Category : <?php the_category(); ?> </h4>
+         <?php get_template_part('template-parts/content', 'blogs'); ?> 
+     </div>
+   <?php get_sidebar(); ?> 
+ </div>
+ <!-- Blog End Here -->
+<?php get_footer(); ?> 
