e70f42fdc3bddda79be19f5d64cba1cf5e74d2bc
Pobitro Deb <pobitrodebnath85@gmail.com>
2024-10-31 05:43:52 +0600

    ACF Repeater Added

---
+++ Projcet_ACF/wp-content/themes/gpoiacf/template-repeater.php
@@ -0,0 +1,37 @@
+<?php
+/*
+    Template Name: Template Repeater
+*/
+get_header(); 
+
+?>
+
+<?php
+    $teams = get_field('team'); 
+?>
+    <?php 
+            foreach($teams as $team){
+                ?> 
+                <div class="single-service">
+                    <h4> <?php echo $team['team_name']; ?>  </h4>
+                    <img src="<?php echo $team['team_image']['url']; ?>" alt="Service 1">
+                    <p><?php echo $team['team_designation']; ?></p>
+                  
+                    <ul>
+                    <?php
+                        $team_social = $team['team_social_profile'];
+
+                        foreach($team_social as $social){
+                        ?>
+                            
+                            <li><a href="<?php echo $social ["social_url_"]['url'] ?>" class="btn" ><i class="<?php echo $social ["social_url_"]['title'] ?>"></i> </a></li>
+                        
+                        <?php
+                        } 
+                    ?>
+                    </ul>
+                   <?php var_dump($social ["social_url_"]['url']); ?>
+                </div>
+                <?php
+            }
+    ?>
