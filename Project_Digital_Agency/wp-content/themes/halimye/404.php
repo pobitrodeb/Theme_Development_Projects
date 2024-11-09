3266c9f1bc49987314804c72fae55e9f17d6a966
Pobitro Deb <pobitrodebnath85@gmail.com>
2024-11-09 16:40:43 +0600

    404 Page Added

---
+++ Project_Digital_Agency/wp-content/themes/halimye/404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?> 
+<section class="breadcumb-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="breadcumb">
+                    <h1> OOPS! This page is not found. </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php get_footer(); ?> 
