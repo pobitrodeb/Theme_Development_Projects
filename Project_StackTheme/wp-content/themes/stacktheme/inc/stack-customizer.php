ea720b5790b1403ff4131d68b446a8ef9c6b3b08
Pobitro Deb <pobitrodebnath85@gmail.com>
2024-11-11 07:50:53 +0600

    About Iteam Repeater Added

--- Project_StackTheme/wp-content/themes/stacktheme/inc/stack-customizer.php
+++ Project_StackTheme/wp-content/themes/stacktheme/inc/stack-customizer.php
@@ -85,7 +85,7 @@ Kirki::add_field('stacktheme_config', [
     'js_vars'           => array(
         array (
           'element'  => '.btn-common',
-			'function' => 'html',
+		  'function' => 'html',
         )
     )
 ]);
@@ -102,4 +102,179 @@ Kirki::add_field('stacktheme_config', [
     'choices'           => [
 		'save_as' => 'array',
 	],
+]);
+
+// About Section Customizer 
+Kirki::add_section('about_section', array(
+    'title'             => esc_html__('About Section', $domain), 
+    'panel'             => 'stacktheme_panel', 
+    'priority'          => 150,
+));
+
+// About Section Title 
+Kirki::add_field('stacktheme_config', [
+    'type'              => 'text', 
+    'settings'          => 'about_heading_text', 
+    'section'           => 'about_section', 
+    'label'             => esc_html__('About Heading Title', $domain), 
+    'default'           => esc_html__('We are helping to grow your business.', $domain),
+    'transport'         => 'postMessage', 
+    'js_vars'           => array(
+        array (
+          'element'     => '.title-hl',
+		  'function'    => 'html',
+        )
+    )
+]);
+
+Kirki::add_field('stacktheme_config',[
+    'type'              => 'typography', 
+    'settings'          => 'about_heading_text_typography', 
+    'section'           => 'about_section',
+    'label'             => esc_html__('About Heading Typography', $domain), 
+    'default'           => [
+          'font-family'     =>  'Titillium Web', 
+          'font-weight'     =>  '700',
+          'font-size'       =>  '38px', 
+          'color'           => '#222222', 
+          'line-height'     => '1.5',
+          'letter-spacing'  => '0',
+          'text-transform'  => 'none',
+          'text-decoration' => 'none',
+          'text-align'      => 'left', 
+    ], 
+    'output'           => [
+        'element'           => '.title-hl', 
+        'function'          => 'html',
+    ] 
+]); 
+
+// About Section Content 
+Kirki::add_field('stacktheme_config', [
+
+    'type'                  => 'textarea', 
+    'settings'              => 'about_section_content', 
+    'section'               => 'about_section', 
+    'label'                 => esc_html__('About Section Content', $domain), 
+    'default'               => esc_html__('A digital studio specialising in User Experience & eCommerce, we combine innovation with digital craftsmanship to help brands fulfill their potential.', $domain), 
+    'transport'             => 'postMessage', 
+    'js_vars'               => array(
+        array(
+            'element'       => '.stack_theme_about_content', 
+            'function'      => 'html'
+        )
+    )
+]); // end kirki about section content 
+
+Kirki::add_field('stacktheme_config', [
+    'type'                  => 'typography', 
+    'settings'              => 'about_section_content_typography', 
+    'section'               => 'about_section', 
+    'label'                 => esc_html__('About Content Typography', $domain), 
+    'default'               => [
+        'font-family'       => 'Open Sans',
+        'font-weight'       => '400', 
+        'font-size'         => '14px',
+        'color'             => '#222222',
+        'line-height'       => '1.5',
+        'letter-spacing'    => '0',
+        'text-transform'    => 'none',
+        'text-decoration'   => 'none',
+        'text-align'        => 'left',   
+    ], 
+    'output'                => array(
+        'element'     =>  '.stack_theme_about_content', 
+        'function'    => 'html'        
+    )
+]);
+
+
+
+Kirki::add_field('stacktheme_config', [
+    'type'          => 'text', 
+    'settings'      => 'about_button_text', 
+    'section'       => 'about_section', 
+    'label'         => esc_html__('About Button Text', $domain), 
+    'default'       => esc_html__('More About Us', $domain), 
+    'transport'     => 'postMessage', 
+    'js_vars'       => array(
+        array(
+            'element'   => '.stacktheme_about_btn', 
+            'function'  => 'html', 
+        ),
+    )
+]); 
+
+Kirki::add_field('stacktheme_config', [
+    'type'          => 'typography', 
+    'settings'      => 'about_button_text_link', 
+    'section'       => 'about_section', 
+    'label'         => esc_html__('About Button Typographiy', $domain), 
+    'default'               => [
+        'font-family'       => 'Open Sans',
+        'font-weight'       => '400', 
+        'font-size'         => '14px',
+        'color'             => '#222222',
+        'line-height'       => '1.5',
+        'letter-spacing'    => '0',
+        'text-transform'    => 'none',
+        'text-decoration'   => 'none',
+        'text-align'        => 'left',   
+    ], 
+    'output'                => array(
+        'element'     =>  '.stacktheme_about_btn', 
+        'function'    => 'html'        
+    )
+]);
+
+// Banner Btn Text 
+Kirki::add_field('stacktheme_config', [
+    'type'              => 'link', 
+    'settings'          => 'about_banner_btn_link', 
+    'section'           => 'about_section', 
+    'label'             => esc_html__('About Button URL', $domain), 
+    'default'           => esc_html__('https://www.facebook.com', $domain),
+    'priority'          => 10,
+    'transport'         => 'postMessage', 
+    'js_vars'           => array(
+        array (
+          'element'  => '.stacktheme_about_btn',
+		  'function' => 'html',
+        )
+    )
+]);
+
+//About Items 
+Kirki::add_field('stacktheme_config', [
+    'type'          => 'repeater', 
+       
+    'section'       => 'about_section',
+    'label'         => esc_html__('About Items', $domain),  
+    'priority'      => 10, 
+    'row_label'     => [
+        'type'  => 'field', 
+        'value' => esc_html__('Add New Item ', $domain), 
+        'field' => 'about_item_title', 
+    ],
+    'button_label'      => esc_html__('Add New About Item', $domain), 
+    'settings'      => 'about_repeater', 
+    'default'      => [
+			[
+				'about_item_title'   => esc_html__( 'What we do', $domain ),
+				'about_item_desc'    => esc_html__( 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia con- sequuntur magni dolores', $domain ),
+				
+			],
+		],
+    'fields'       => [
+			'about_item_title'   => [
+				'type'        => 'text',
+				'label'       => esc_html__( 'About Item Title', 'kirki' ),
+				'default'     => '',
+			],
+			'about_item_desc'    => [
+				'type'        => 'textarea',
+				'label'       => esc_html__( 'About Item Description', 'kirki' ),
+				'default'     => '',
+			],
+		],
 ]);
