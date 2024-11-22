65cb3cb72d9afac63dff056db9cda3becb2649eb
Pobitro Deb <pobitrodebnath85@gmail.com>
2024-11-22 06:32:27 +0600

    ACF Plugin Install Check Added

--- Project_StartupTheme/wp-content/themes/startuptheme/index.php
+++ Project_StartupTheme/wp-content/themes/startuptheme/index.php
@@ -3,35 +3,37 @@
         <div id="header-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <?php
-                    $args = array(
-                        'post_type'             => 'sliders', 
-                        'posts_per_page'        => 3, 
-                    );
-                    $query = new WP_Query($args); 
-                    $i = 0; 
-                    if($query->have_posts()){
-                        while($query->have_posts()){
-                            $query->the_post();
-                            $i++; 
-                            $sub_heading =  get_field('sub_heading');
-                            $slider_button_title_1 = get_field('slider_button_title_1');
-                            $slider_button_title_2 = get_field('slider_button_title_2');
-                            $slider_button_title_2 = get_field('slider_button_title_2');
-                            $slider_button_1_url = get_field('slider_button_1_url');
-                            $slider_button_2_url_ = get_field('slider_button_2_url_');
-                            ?>
-                                <div class="carousel-item <?php if($i == 1 ){ echo 'active';} ?>">
-                                    <img class="w-100" src="<?php the_post_thumbnail_url();?>" alt="Image">
-                                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                                        <div class="p-3" style="max-width: 900px;">
-                                            <h5 class="text-white text-uppercase mb-3 animated slideInDown"><?php echo $sub_heading; ?></h5>
-                                            <h1 class="display-1 text-white mb-md-4 animated zoomIn"><?php echo the_title(); ?></h1>
-                                            <a href="<?php echo  $slider_button_1_url; ?>" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft"><?php echo $slider_button_title_1; ?></a>
-                                            <a href="<?php echo  $slider_button_2_url_; ?>" class="btn btn-outline-light py-md-3 px-md-5 animated slideInRight"><?php echo $slider_button_title_2; ?></a>
+                    if(class_exists('ACF')){  //ACF Plugin Install Check 
+                        $args = array(
+                            'post_type'             => 'sliders', 
+                            'posts_per_page'        => 3, 
+                        );
+                        $query = new WP_Query($args); 
+                        $i = 0; 
+                        if($query->have_posts()){
+                            while($query->have_posts()){
+                                $query->the_post();
+                                $i++; 
+                                $sub_heading =  get_field('sub_heading');
+                                $slider_button_title_1 = get_field('slider_button_title_1');
+                                $slider_button_title_2 = get_field('slider_button_title_2');
+                                $slider_button_title_2 = get_field('slider_button_title_2');
+                                $slider_button_1_url = get_field('slider_button_1_url');
+                                $slider_button_2_url_ = get_field('slider_button_2_url_');
+                                ?>
+                                    <div class="carousel-item <?php if($i == 1 ){ echo 'active';} ?>">
+                                        <img class="w-100" src="<?php the_post_thumbnail_url();?>" alt="Image">
+                                        <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                                            <div class="p-3" style="max-width: 900px;">
+                                                <h5 class="text-white text-uppercase mb-3 animated slideInDown"><?php echo $sub_heading; ?></h5>
+                                                <h1 class="display-1 text-white mb-md-4 animated zoomIn"><?php echo the_title(); ?></h1>
+                                                <a href="<?php echo  $slider_button_1_url; ?>" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft"><?php echo $slider_button_title_1; ?></a>
+                                                <a href="<?php echo  $slider_button_2_url_; ?>" class="btn btn-outline-light py-md-3 px-md-5 animated slideInRight"><?php echo $slider_button_title_2; ?></a>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            <?php
+                                <?php
+                            }
                         }
                     }
                 ?>
